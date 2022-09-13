1984a91b1c0f77d2c3d4408855daf9fc4b3d61ba
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-13 20:48:13 +0700

    first commit

---
+++ bai6.js
@@ -0,0 +1,105 @@
+function picture(){
+    let x=Math.floor(Math.random() * 5);
+    switch (x){
+        case 0:
+            result="./hinh anh/2.jfif";
+            break;
+        case 1:
+            result="./hinh anh/3.jfif";
+            break;
+        case 2:
+            result="./hinh anh/1.jfif";
+            break;
+        case 3:
+            result="./hinh anh/4.jfif";
+            break;
+        case 4:
+            result="./hinh anh/5.jfif";
+            break;
+    }
+    document.getElementById('img1').src=result;
+}
+function picture1(){
+    let x=Math.floor(Math.random() * 5);
+    switch (x){
+        case 0:
+            result="./hinh anh/2.jfif";
+            break;
+        case 1:
+            result="./hinh anh/3.jfif";
+            break;
+        case 2:
+            result="./hinh anh/1.jfif";
+            break;
+        case 3:
+            result="./hinh anh/4.jfif";
+            break;
+        case 4:
+            result="./hinh anh/5.jfif";
+            break;
+    }
+    document.getElementById('img2').src=result;
+}
+function picture2(){
+    let x=Math.floor(Math.random() * 5);
+    switch (x){
+        case 0:
+            result="./hinh anh/2.jfif";
+            break;
+        case 1:
+            result="./hinh anh/3.jfif";
+            break;
+        case 2:
+            result="./hinh anh/1.jfif";
+            break;
+        case 3:
+            result="./hinh anh/4.jfif";
+            break;
+        case 4:
+            result="./hinh anh/5.jfif";
+            break;
+    }
+    document.getElementById('img3').src=result;
+}
+function picture3(){
+    let x=Math.floor(Math.random() * 5);
+    switch (x){
+        case 0:
+            result="./hinh anh/2.jfif";
+            break;
+        case 1:
+            result="./hinh anh/3.jfif";
+            break;
+        case 2:
+            result="./hinh anh/1.jfif";
+            break;
+        case 3:
+            result="./hinh anh/4.jfif";
+            break;
+        case 4:
+            result="./hinh anh/5.jfif";
+            break;
+    }
+    document.getElementById('img4').src=result;
+}
+function picture4(){
+    let x=Math.floor(Math.random() * 5);
+    switch (x){
+        case 0:
+            result="./hinh anh/2.jfif";
+            break;
+        case 1:
+            result="./hinh anh/3.jfif";
+            break;
+        case 2:
+            result="./hinh anh/1.jfif";
+            break;
+        case 3:
+            result="./hinh anh/4.jfif";
+            break;
+        case 4:
+            result="./hinh anh/5.jfif";
+            break;
+    }
+    document.getElementById('img5').src=result;
+}
